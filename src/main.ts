94ea84c28db13b7b2ef8cbbf376f96e0fe0c5821
Hai Chang <haichang@microsoft.com>
2023-04-16 21:17:47 +0800

    Update API version handling and READMEs

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['log'],
+    logger: ['debug'],
   });
   app.setGlobalPrefix('v1');
   await app.listen(3000);
