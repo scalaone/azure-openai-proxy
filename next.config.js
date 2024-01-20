16eeb7414b0a4c596bcc3e368dc0d2029bf656b9
Blair Chen <blrchen@users.noreply.github.com>
2024-01-21 00:50:43 +0800

    Upgrade to Next.js 14.1.0 (#27)
    - Upgrade to Next.js 14.1.0
    - Upgrade Dockerfile use Node.js 20 base image

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
